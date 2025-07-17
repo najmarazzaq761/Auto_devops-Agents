4755e5f5eaa8ad43aba744aef8eb3c71d8fae431
Najma Razzaq <148422805+najmarazzaq761@users.noreply.github.com>
2025-07-18 01:43:58 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import AutoDevOpsAgent from "./components/AutoDevOpsAgent.jsx";
+import "./index.css";
+
+function App() {
+  return <AutoDevOpsAgent />;
+}
+
+export default App;
